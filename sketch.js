5d04904eb5c74575ffdeafad3ec13e231ba72de9
kairavee <74608690+kairavee@users.noreply.github.com>
2021-01-03 19:32:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var ground1,ground2
+
+function preload() 
+{
+
+}
+
+function setup()
+{
+  Canvas=600,600
+
+}
+
+function draw()
+{
+
+}
+
